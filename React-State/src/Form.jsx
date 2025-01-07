d37f6a974c282da43f44a1580b6c4b98c88b96b9
Adya Kumari <adyajha776@gmail.com>
2025-01-08 01:42:31 +0530

    Labels in form & Common Handler for multiple input

--- React-State/src/Form.jsx
+++ React-State/src/Form.jsx
@@ -1,15 +1,36 @@
 import { useState } from "react";
 
 export default function Form() {
-    let [fullName, setFullName] = useState("Adya");
+    let [formData, setFormData] = useState({
+        fullName: "",
+        username: "",
+    });
 
-    let handleNameChange = (event) => {
-        setFullName(event.target.value);
+    let handleInputChange = (event) => {
+        setFormData((currData) => {
+            return { ...currData, [event.target.name]: event.target.value};
+        });
+    };
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+        setFormData({
+            fullName: "",
+            username: "",
+        });   
     };
 
     return (
-        <form>
-            <input placeholder="Enter Your Name " type="text" value={fullName} onChange={handleNameChange} />
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="fullName">Full Name</label>
+            <input placeholder="Enter Your Full Name " type="text" value={formData.fullName} onChange={handleInputChange} name="fullName" id="fullName"/>
+            <br /><br />
+            <br /><br />
+            <label htmlFor="username">Username</label>
+            <input placeholder="Enter username" type="text" value={formData.username} onChange={handleInputChange} name="username" id="username"/>
+            <br /><br />
+            <br /><br />
             <button type="submit">Submit</button>
         </form>
     );
